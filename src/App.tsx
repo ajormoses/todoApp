9c2098b031e8b1a36210daea5dad90e448c81d5c
ajormoses <ajormoses@gmail.com>
2025-01-03 16:19:22 +0100

    feat: Add App component with TodoApp integration

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./assets/tailwind.css";
+import "@mdi/font/css/materialdesignicons.css";
+import TodoApp from "./components/TodoApp";
+
+function App() {
+  return (
+    <>
+      <TodoApp />
+    </>
+  );
+}
+
+export default App;
