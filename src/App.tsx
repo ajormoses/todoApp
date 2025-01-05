a8d8618ed157a4b2d8a5eb340bea819c25974e99
ajormoses <ajormoses@gmail.com>
2025-01-05 13:10:19 +0100

    refactor: Remove TodoApp component from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import "./assets/tailwind.css";
 import "@mdi/font/css/materialdesignicons.css";
-import TodoApp from "./components/TodoApp";
 
 function App() {
-  return (
-    <>
-      <TodoApp />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
