13680f99583f5ce48ffa92adeeec1d37e8cb0de9
ajormoses <ajormoses@gmail.com>
2025-01-08 13:07:42 +0100

    feat: Add TodoProvider to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./assets/tailwind.css";
 import "@mdi/font/css/materialdesignicons.css";
 import TodoApp from "./components/TodoApp";
+import { TodoProvider } from "./components/context/TodoContext";
 
 function App() {
   return (
     <>
-      <TodoApp />
+      <TodoProvider>
+        <TodoApp />
+      </TodoProvider>
     </>
   );
 }
