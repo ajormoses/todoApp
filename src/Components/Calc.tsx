677e58c98d5b8304e5075ee02e32da9e55a96cc4
ajormoses <ajormoses@gmail.com>
2025-01-09 08:58:28 +0100

    feat: Add Calc component to App.tsx

---
+++ src/Components/Calc.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Calc = () => {
+  return (
+    <div className="container">
+      <div className="flex items-center justify-center h-screen">
+        <div className="rounded-lg p-4 bg-white w-full max-w-[700px] flex flex-col gap-4"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Calc;
